f976150f4cd0c939bdb81244f7c275e055d4820a
anor4k <noel.eliezer@pm.me>
2020-06-09 17:31:32 -0300

    format setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = 'climuxer',
-    version = '0.1.0',
-    py_modules = ['climuxer'],
-    install_requires = [
+    name='climuxer',
+    version='0.1.0',
+    py_modules=['climuxer'],
+    install_requires=[
         'click',
         'pymediainfo',
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'climuxer = climuxer:main',
         ]
